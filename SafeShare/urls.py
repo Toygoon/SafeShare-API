11c91a9fdb86d1bcb4b9b24fb48165989a1a9359
Toygoon <toygoon@gmail.com>
2023-03-20 21:02:43 +0900

    [Add] api 앱에 view 연결

--- SafeShare/urls.py
+++ SafeShare/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
+import api.urls
 
 # OpenAPI 관련 설정
 schema_view = get_schema_view(
@@ -32,6 +34,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
 
