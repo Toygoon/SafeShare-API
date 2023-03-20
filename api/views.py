04885e6a76362eb8e4e66f882ec502bd960bcf97
Toygoon <toygoon@gmail.com>
2023-03-20 21:03:49 +0900

    [Add] 로그인용 LoginView API 추가

--- api/views.py
+++ api/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.authtoken.admin import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.serializer import UserSerializer
+
 
 # Create your views here.
+
+class LoginView(APIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        # User inputs the ID, and Password
+        user_id = request.data.get('user_id')
+        user_pw = request.data.get('user_pw')
+
+        # If the user input has some errors
+        if user_id is None or user_pw is None \
+                or len(user_id) < 2 or len(user_pw) < 2:
+            return Response({'error': 'input error'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Find the user from table
+        user = None
+        try:
+            user = User.objects.all.filter(user_id=user_id, user_pw=user_pw)
+        except:
+            pass
+
+        # Failed to find user, or password error
+        if user is None:
+            return Response({'error': 'authentication error'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response({'result': 'ok'}, status=status.HTTP_200_OK)
