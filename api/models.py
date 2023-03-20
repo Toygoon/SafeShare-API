29afbe7a1a4cde6a1ad6cd406c66d2c7d8de8ab9
Toygoon <toygoon@gmail.com>
2023-03-20 21:03:21 +0900

    [Add] User 모델 추가

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    user_id = models.CharField(max_length=10)
+    user_pw = models.CharField(max_length=50)
+    name = models.CharField(max_length=10)
+    mileage = models.PositiveIntegerField()
+    mobile = models.CharField(max_length=50)
