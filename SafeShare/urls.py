a0727bbfd9f1f5e21fa841d5d4dff243bf4d4b8d
Toygoon <toygoon@gmail.com>
2023-03-20 15:04:45 +0900

    [Add] urls.py에서 redoc을 mapping 하도록 설정

--- SafeShare/urls.py
+++ SafeShare/urls.py
@@ -13,9 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+# OpenAPI 관련 설정
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SafeShare-API",
+        default_version="0.0.1",
+        description="SafeShare API 문서",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
