e58874871d6a16a3bbbde092899618b6cacadac6
Toygoon <toygoon@gmail.com>
2023-03-21 14:44:43 +0900

    [Edit] LoginView API 작성

--- api/serializer.py
+++ api/serializer.py
@@ -4,6 +4,10 @@ from api.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    User 객체의 직렬화 클래스
+    """
+
     class Meta:
         model = User
         fields = ['user_id', 'user_pw']
--- api/views/LoginView.py
+++ api/views/LoginView.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.authtoken.admin import User
 from rest_framework.response import Response
@@ -7,9 +9,45 @@ from api.serializer import UserSerializer
 
 
 class LoginView(APIView):
+    """
+    로그인을 담당하는 API
+
+    Args:
+        request (Request): id, pw를 입력받기 위한 HttpRequest
+    """
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    @swagger_auto_schema(
+        operation_id='POST /login',
+        operation_description='(로그인) POST 요청으로 아이디와 비밀번호를 입력받고, 성공 여부를 반환합니다.',
+        manual_parameters=[openapi.Parameter('user_id', openapi.IN_PATH, description='아이디', required=True,
+                                             type=openapi.TYPE_STRING),
+                           openapi.Parameter('user_pw', openapi.IN_PATH, description='비밀번호', required=True,
+                                             type=openapi.TYPE_STRING)],
+        responses={200: openapi.Response(
+            description='로그인 성공',
+            examples={
+                'application/json': {
+                    'result': 'ok'
+                }
+            }
+        ), 400: openapi.Response(
+            description='입력된 값이 잘못됨',
+            examples={
+                'application/json': {
+                    'error': 'input error'
+                }
+            }
+        ), 401: openapi.Response(
+            description='로그인 실패',
+            examples={
+                'application/json': {
+                    'error': 'authentication error'
+                }
+            })
+        })
     def post(self, request):
         # User inputs the ID, and Password
         user_id = request.data.get('user_id')
