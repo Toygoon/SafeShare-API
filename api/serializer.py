123565d80026092d66f43be1a9b94a7d2e413eeb
Toygoon <toygoon@gmail.com>
2023-03-21 17:05:30 +0900

    [Add] 회원가입 API에 사용될 RegisterSerializer 생성

--- api/serializer.py
+++ api/serializer.py
@@ -11,3 +11,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['user_id', 'user_pw']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """
+    RegisterView에서 쓰이는 회원가입용 직렬화 클래스
+    """
+
+    class Meta:
+        model = User
+        fields = ['user_id', 'user_pw', 'name', 'mobile']
