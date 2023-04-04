d19c6feaf3073a31bb987b807dcb802e6ee12c9c
Toygoon <toygoon@gmail.com>
2023-04-04 14:19:01 +0900

    [Add] token api를 url에 연결

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from api import views
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
-    path('risk_report/', views.RiskReportView.as_view(), name='risk-report')
+    path('risk_report/', views.RiskReportView.as_view(), name='risk-report'),
+    path('token/', views.AppTokenView.as_view(), name='token')
 ]
