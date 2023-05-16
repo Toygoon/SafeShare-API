287657800bcc9f4f257e9fb24f049d287f87afc9
Toygoon <toygoon@gmail.com>
2023-05-16 16:05:21 +0900

    [Add] DeviceToken이 Admin에서 뜨도록 설정

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from api.models import DeviceToken
+
+
 # Register your models here.
+class DeviceTokenAdmin(admin.ModelAdmin):
+    list_display = ['user', 'token']
+
+
+admin.site.register(DeviceToken, DeviceTokenAdmin)
