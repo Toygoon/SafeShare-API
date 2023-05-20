09b83ac61d99e0f5161b978ac223bd40033c4e92
Toygoon <toygoon@gmail.com>
2023-05-20 12:03:12 +0900

    [Edit] Admin 페이지에서 Token 대신 User가 뜨도록 수정

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from api.models import DeviceToken
+from api.models import User
 
 
 # Register your models here.
-class DeviceTokenAdmin(admin.ModelAdmin):
-    list_display = ['user', 'token']
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['user_id', 'user_pw', 'name', 'mileage', 'mobile', 'registered_at', 'app_token']
 
 
-admin.site.register(DeviceToken, DeviceTokenAdmin)
+admin.site.register(User, UserAdmin)
