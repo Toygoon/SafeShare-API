2e30a1bf81ddf9fa3c00ee9617baac5543def45c
Toygoon <toygoon@gmail.com>
2023-06-07 17:20:05 +0900

    [Edit] UserAction 변수 이름 수정, start_time이 자동으로 현재 시간이 되도록 수정

--- api/models/UserAction.py
+++ api/models/UserAction.py
@@ -6,14 +6,14 @@ class UserAction(models.Model):
     사용자의 재난 요청을 저장하는 Model
 
     Args:
-        startTime (DateTimeField): 시작 시간
-        endTime (DateTimeField): 종료 시간
+        start_time (DateTimeField): 시작 시간
+        end_time (DateTimeField): 종료 시간
         user (ForeignKey): User의 외래키
         latlng (ForeignKey): LatLng의 외래키
     """
 
-    startTime = models.DateTimeField(auto_now=False, default=None, null=True)
-    endTime = models.DateTimeField(auto_now=False, default=None, null=True)
+    start_time = models.DateTimeField(auto_now=True)
+    end_time = models.DateTimeField(auto_now=False, default=None, null=True)
     user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True, default=None)
     latlng = models.ForeignKey('LatLng', on_delete=models.SET_NULL, null=True, default=None)
 
