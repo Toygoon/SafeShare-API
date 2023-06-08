d522d0035022524331bb61b581a49f333c67a09f
Toygoon <toygoon@gmail.com>
2023-06-08 17:31:24 +0900

    [Add] 사용자에 전문 요원 변수 추가

--- api/models/User.py
+++ api/models/User.py
@@ -22,6 +22,7 @@ class User(models.Model):
     mobile = models.CharField(max_length=50)
     registered_at = models.DateTimeField(auto_now_add=True)
     app_token = models.TextField(max_length=500)
+    is_paramedic = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'user'
